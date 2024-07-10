4a4fa4d75124c8a16130a52f0d62461965c81398
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2024-07-10 07:34:51 +0000

    modified:   BreakingNews.csv 	modified:   australia2.py

--- australia2.py
+++ australia2.py
@@ -4,28 +4,36 @@ from bs4 import BeautifulSoup
 import pycountry
 import datetime
 
-url ="https://www.news.com.au/national/breaking-news/page/3"
+url ="https://www.news.com.au/national/breaking-news/page/"
 
 loca=[]
 date=[]
 title=[]
+url1=[]
 category=[]
 
-def HTML():
-    code=requests.get(url).content
+def HTML(i):
+    code=requests.get(url+str(i)).content
     soup=BeautifulSoup(code,"lxml")
     newfind=soup.find("div",class_="list l1-s")
     data=newfind.find_all("article",class_="storyblock")
     for dat in data:
+        country="Australia"
         try:
-            title.append(dat.find("h4",class_="storyblock_title").text)
+            titles=dat.find("h4",class_="storyblock_title")
+            title.append(titles.text)
+            
+            urls=titles.find('a')
+            url1.append(urls.get('href'))
         except:
             title.append("")
+            url1.append("")
         try:
             category.append(dat.find("span",class_="storyblock_meta").text)
         except:
             category.append("")
         get_countries(dat.find("p",class_="storyblock_standfirst g_font-body-s"))
+        loca.append(country)
         try:
             date.append(datetime.datetime.now().date())
         except:
@@ -34,20 +42,22 @@ def HTML():
         "title":title,
         "category":category,
         "date":date,
-        "location":loca
+        "location":loca,
+        "url":url1
     }
     new_csv(dic)
 
 
 def get_countries(body):
+    print(len(pycountry.countries))
     for x in pycountry.countries:
         if x.name in body:
-            loca.append(x.name)
+            country=x.name
             break
-    loca.append("Australia")
     
 def new_csv(file):
     df=pd.DataFrame(file)
     df.to_csv("BreakingNews.csv")
 
-HTML()
+for i in range(1,10):
+    HTML(i)
